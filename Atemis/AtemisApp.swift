3546dbdbe5e6b4c4c25350b378f4e8fbebbccf09
Nicholas <nichz123@gmail.com>
2021-10-26 16:19:27 +0700

    Initial Commit

---
+++ Atemis/AtemisApp.swift
@@ -0,0 +1,20 @@
+//
+//  AtemisApp.swift
+//  Atemis
+//
+//  Created by Nicholas on 26/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AtemisApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
