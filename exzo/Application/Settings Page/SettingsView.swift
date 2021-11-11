1b73a8c33a8a79547f6bb128768b4d6f0e5bc45b
Natval <devennatval@gmail.com>
2021-11-11 23:02:57 +0700

    Add Setting View Page

---
+++ exzo/Application/Settings Page/SettingsView.swift
@@ -0,0 +1,73 @@
+//
+//  SettingsView.swift
+//  exzo
+//
+//  Created by Deven Nathanael on 11/11/21.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        VStack {
+            ProfileHeaderView(profileImage: "pp_004", profileName: "Deven Nathanael")
+                .padding(.bottom)
+            List {
+                Section(header: Text("REMINDER")) {
+                    SettingRowView(settingIcon: "timer", settingTitle: "Set Reminder")
+                }
+                .listRowBackground(Color.init(red: 242/255, green: 242/255, blue: 242/255))
+                
+                Section(header: Text("LANGUAGE")) {
+                    SettingRowView(settingIcon: "globe", settingTitle: "Language")
+                }
+                .listRowBackground(Color.init(red: 242/255, green: 242/255, blue: 242/255))
+                
+                Section(header: Text("LEGAL MATTERS")) {
+                    SettingRowView(settingIcon: "lock", settingTitle: "Privacy Policy")
+                    SettingRowView(settingIcon: "exclamationmark.triangle", settingTitle: "Terms and Conditions")
+                }
+                .listRowBackground(Color.init(red: 242/255, green: 242/255, blue: 242/255))
+                
+                Section(header: Text("OTHERS")) {
+                    SettingRowView(settingIcon: "star", settingTitle: "Rate our App")
+                    SettingRowView(settingIcon: "questionmark.circle", settingTitle: "Frequently Asked Questions")
+                    SettingRowView(settingIcon: "phone", settingTitle: "Contact Us")
+                    SettingRowView(settingIcon: "trash", settingTitle: "Delete Account")
+                    SettingRowView(settingIcon: "arrowshape.turn.up.backward", settingTitle: "Sign Out")
+                    
+                }
+                .listRowBackground(Color.init(red: 242/255, green: 242/255, blue: 242/255))
+                
+            }
+            .onAppear {
+                UITableView.appearance().backgroundColor = .white
+            }
+            .onDisappear {
+                UITableView.appearance().backgroundColor = .systemGray
+            }
+            .cornerRadius(30)
+        }
+    }
+    
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
+
+struct NavigationConfigurator: UIViewControllerRepresentable {
+    var configure: (UINavigationController) -> Void = { _ in }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<NavigationConfigurator>) -> UIViewController {
+        UIViewController()
+    }
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<NavigationConfigurator>) {
+        if let nc = uiViewController.navigationController {
+            self.configure(nc)
+        }
+    }
+
+}
